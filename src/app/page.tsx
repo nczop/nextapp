a54817c18bb53b652e499c97f8cd8a7c34f7c5f2
Natalia <naati.czop@gmail.com>
2024-03-21 16:47:40 +0100

    added ThemeProvider for dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-	return <div>Home Page</div>;
+	return (
+		<>
+			<div>Home Page</div>
+			<Button>Button</Button>
+		</>
+	);
 }
